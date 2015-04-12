bb970699000bd19d85c69700e867d922d62fc233
Amit Bapat <abapat@Amits-MacBook-Pro.local>
2015-04-12 02:34:21 -0400

    added push functionality

---
+++ GCM.php
@@ -0,0 +1,93 @@
+<?php
+
+function sendPushNotif($regID, $api) {
+
+	$ids = array("$regID");
+
+    $url = 'https://android.googleapis.com/gcm/send';
+
+    //------------------------------
+    // Set GCM post variables
+    // (Device IDs and push payload)
+    //------------------------------
+
+    $post = array('registration_ids' => $ids);
+
+    //------------------------------
+    // Set CURL request headers
+    // (Authentication and type)
+    //------------------------------
+
+    $headers = array( 
+                        'Authorization: key=' . $api,
+                        'Content-Type: application/json'
+                    );
+
+    //------------------------------
+    // Initialize curl handle
+    //------------------------------
+
+    $ch = curl_init();
+
+    //------------------------------
+    // Set URL to GCM endpoint
+    //------------------------------
+
+    curl_setopt( $ch, CURLOPT_URL, $url );
+
+    //------------------------------
+    // Set request method to POST
+    //------------------------------
+
+    curl_setopt( $ch, CURLOPT_POST, true );
+
+    //------------------------------
+    // Set our custom headers
+    //------------------------------
+
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+
+    //------------------------------
+    // Get the response back as 
+    // string instead of printing it
+    //------------------------------
+
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+    //------------------------------
+    // Set post data as JSON
+    //------------------------------
+
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( $post ) );
+
+    //------------------------------
+    // Actually send the push!
+    //------------------------------
+
+    $result = curl_exec( $ch );
+
+    //------------------------------
+    // Error? Display it!
+    //------------------------------
+
+    if ( curl_errno( $ch ) )
+    {
+        echo 'GCM error: ' . curl_error( $ch );
+    }
+
+    //------------------------------
+    // Close curl handle
+    //------------------------------
+
+    curl_close( $ch );
+
+    return $result;
+}
+
+$id = "APA91bEPZoxREVUVYwNFZkvLJf_fLQRmRH6ntu_J-kJMFruEYCp3eSMcgl2zm-8YtDGsA8QugUem9lb7hCr1nvrXKn5iTP-E_22YAdJcOp_uD0zNr41cA2WyNnYXWoRub1eZAGWHzFxrzgN5nwdY6JzZ6B28q3bCgA";
+$api = "AIzaSyB6hhLczi2wf0_G5UGaSltnAL459pVSV2A";
+
+$res = sendPushNotif($id, $api);
+
+var_dump($res);
+?>
